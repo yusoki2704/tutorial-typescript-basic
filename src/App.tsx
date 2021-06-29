6e8790d0757636251ca390968f392bc5526d2d01
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-06-29 20:14:19 +0900

    add Practice4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
+import { Practice4 } from "./practices/Practice4";
 
 import "./styles.css";
 
@@ -10,6 +11,7 @@ export default function App() {
       <Practice1 />
       <Practice2 />
       <Practice3 />
+      <Practice4 />
     </div>
   );
 }
